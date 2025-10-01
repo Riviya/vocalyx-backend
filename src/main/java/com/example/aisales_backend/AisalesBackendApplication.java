aaab32454f180eb88e1d06d1f040e3eed38fff27
Riviya <104679173+Riviya@users.noreply.github.com>
2025-10-01 18:07:48 +0530

    Update AisalesBackendApplication.java

--- src/main/java/com/example/aisales_backend/AisalesBackendApplication.java
+++ src/main/java/com/example/aisales_backend/AisalesBackendApplication.java
@@ -2,12 +2,14 @@ package com.example.aisales_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.example.aisales_backend.entity")  // Add this line
+@EnableJpaRepositories("com.example.aisales_backend.repository")  // Add this line
 public class AisalesBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AisalesBackendApplication.class, args);
     }
-
 }
